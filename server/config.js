b36a4686b0a02f284edc8d5931cd1ed54aa04686
Emery <patemery@bu.edu>
2017-07-29 21:49:55 -0400

    Added Components, Node Server, Routing, Auth0 API, MongoDB config

---
+++ server/config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  AUTH0_DOMAIN: 'patemery.auth0.com',
+  AUTH0_API_AUDIENCE: 'http://localhost:8083/api/', /* auth0 audience */
+  MONGO_URI: process.env.MONGO_URI || 'mongodb://adminpat:A44les14@ds129153.mlab.com:29153/rsvp-mean'
+};
